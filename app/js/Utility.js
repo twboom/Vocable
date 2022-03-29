cc5e2b6240ee32c5e6c0903422b50ff216474118
Thijs Boom <twboom@hotmail.nl>
2022-03-29 18:16:18 +0200

    Cleanup in shuffle function

--- app/js/Utility.js
+++ app/js/Utility.js
@@ -4,14 +4,14 @@ export function shuffle(array) {
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
   
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
   
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
   
     return array;
-  }
+}
