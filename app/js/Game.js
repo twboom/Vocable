18dea3569dc3f62ff25762d07286034345d83132
Thijs Boom <twboom@hotmail.nl>
2022-04-11 21:42:19 +0200

    Implemented score-based next word system

--- app/js/Game.js
+++ app/js/Game.js
@@ -1,43 +1,59 @@
 import { Field } from './Field.js';
 import { Keyboard } from './Keyboard.js';
-import { shuffle } from './Utility.js';
+import { shuffle, getRandomScore, getRandomFromScore, getMinScore } from './Utility.js';
 
 export class Game {
-    #currentWordIndex = 0;
+    scores = {}; // Object with the scores per word
     
-    constructor(wordList, doShuffle = true) {
+    constructor(wordList, doShuffle = true, scoreList = {}) {
         this.words = wordList;
 
-        if (doShuffle) { this.words = shuffle(this.words) };
+        // Add the words to the scores object
+        for (let word of wordList) {
+            this.scores[word.word] = 0;
+        };
 
+        // Mix with the given score list
+        this.scores = { ...this.scores, ...scoreList };
+
+        console.log(this.scores)
+        
         this.field = new Field(this);
         this.keyboard = new Keyboard();
         this.keyboard.attach(this.field);
     };
-
+    
     // Start the game
     start() {
-        this.field.update(this.words[this.#currentWordIndex]);
-        this.keyboard.generate();
+        this.next();
     };
-
+    
     // Go to the next word
     next() {
-        this.#currentWordIndex++;
-        this.field.update(this.words[this.#currentWordIndex]);
+        // Get a new score to choose a word from
+        const randomScore = getRandomScore(getMinScore(this.scores), 0.1);
+        
+        // Get a new word
+        const newWord = getRandomFromScore(this.scores, randomScore); // Get the word
+        const newWordIndex = this.words.findIndex(word => word.word === newWord); // Get the index of the word
+        
+        // Update the field with the new word
+        this.field.update(this.words[newWordIndex]);
     };
 
     // Win scenario
     win(guesses) {
         // TODO
-        this.next();
         console.log(`Won in ${guesses} guesses`);
+        this.scores[this.field.word.word] = guesses;
+        this.next();
     };
     
     // Lose scenario
     lose() {
         // TODO
+        console.log(`Lost, word was ${this.field.word.word}`);
+        this.scores[this.field.word.word] = 0;
         this.next();
-        console.log('Lost');
     };
 };
