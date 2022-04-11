c17d7e3fdace95e87bfcb7cbd19ea11c85ae4d68
Thijs Boom <twboom@hotmail.nl>
2022-04-11 23:40:42 +0200

    Test with build

--- build.py
+++ build.py
@@ -48,6 +48,18 @@ if __name__ == "__main__":
         table_html = build_table(file)
         export_table_page(file, table_html)
 
+    
+    def list_files(startpath):
+        for root, dirs, files in os.walk(startpath):
+            level = root.replace(startpath, '').count(os.sep)
+            indent = ' ' * 4 * (level)
+            print('{}{}/'.format(indent, os.path.basename(root)))
+            subindent = ' ' * 4 * (level + 1)
+            for f in files:
+                print('{}{}'.format(subindent, f))
+
+    
+    list_files('build')
 
     # Copy static files
     print('[*] Copying static files')
